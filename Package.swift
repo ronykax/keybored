cbed3ea4a470279dd567195cd9eb6af13672b525
ronykax <ronykax@gmail.com>
2026-05-12 16:01:24 +0530

    yes

--- Package.swift
+++ Package.swift
@@ -9,8 +9,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "TapSH",
-            path: "TapSH"
+            name: "TapSH"
         )
     ],
     swiftLanguageModes: [.v6]
